6117f5ede3bea84a1cc262147ff47ecd842e1c5a
mondragon365 <lord_drako07@hotmail.com>
2020-08-17 17:14:12 -0500

    ok

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
-const AWS = require('aws-sdk');
-var translate = new AWS.Translate({ apiVersion: '2017-07-01' });
-const BaseService = require('./service');
-const Helpert = require('./helpert');
+
 const Router = require('./api/routes/index');
 const bodyParser = require('body-parser');
 
@@ -15,58 +12,4 @@ app.use(bodyParser.json({ strict: false }));
 var router = new Router();
 app.use('/', router);
 
-// app.get('/api/peoplee/:id', async function (req, res) {
-//   var id = req.params.id;
-//   var servicio = new BaseService();
-//   var { data } = await servicio.obtener(id);
-//   var helpert = new Helpert();
-//   var trasn = await helpert.translate_string(data);
-//   res.send(trasn);
-
-//   //console.log(dat.data);
-//   //res.send(dat.data);
-
-
-//   // var params = {
-//   //   SourceLanguageCode: 'en', /* required */
-//   //   TargetLanguageCode: 'es', /* required */
-//   //   Text: "name,height,mass,hair color,skin color,eye color,birth year,gender,homeworld,films,species,vehicles,starships,created,edited,url", /* required */
-//   // };
-//   // translate.translateText(params, function (err, data) {
-//   //   console.log(123);
-//   //   console.log(data.TranslatedText);
-//   //   res.send(data.TranslatedText);
-//   // });
-
-//   // var helpert = new Helpert();
-//   // var trasn = await helpert.translate_string();
-//   // res.send(id);
-//   // var servicio = new BaseService();
-//   // var dat = await servicio.obtener();
-//   // console.log(dat.data);
-//   // res.send(dat.data);
-// });
-
-// app.get('/api/films/:id', async function (req, res) {
-//   var id = req.params.id;
-//   var servicio = new BaseService();
-//   var { data } = await servicio.obtenerFilms(id);
-//   var helpert = new Helpert();
-//   var trasn = await helpert.translate_string(data);
-//   res.send(trasn);
-// })
-
 module.exports.handler = serverless(app);
-
-// exports.handler = (event) => {
-//   var params = {
-//     SourceLanguageCode: 'en', /* required */
-//     TargetLanguageCode: 'it', /* required */
-//     Text: 'my text here', /* required */
-//   };
-//   translate.translateText(params, function(err, data) {
-//       console.log(123);
-//     console.log(data);
-//   });
-
-// };
